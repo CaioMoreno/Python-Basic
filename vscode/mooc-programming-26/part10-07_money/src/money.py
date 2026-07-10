889cfdd73737ae0f22d9082b18eb14f58a7fd67c
Caio <caiomoreno.caio@gmail.com>
2026-07-10 18:19:52 -0300

    New activities

--- vscode/mooc-programming-26/part10-07_money/src/money.py
+++ vscode/mooc-programming-26/part10-07_money/src/money.py
@@ -1,9 +1,52 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self.__euros = euros
+        self.__cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        if self.__cents>=10:
+            return f"{self.__euros}.{self.__cents} eur" 
+        else:
+            return f"{self.__euros}.0{self.__cents} eur"
+        
+    def __eq__(self, value):
+        return self.__cents == value.__cents and self.__euros == value.__euros
+    
+    def __ne__(self, value):
+        return self.__cents != value.__cents or self.__euros != value.__euros
+    
+    def __lt__(self, other):
+        if self.__euros == other.__euros:
+            return self.__cents < other.__cents
+        return self.__euros < other.__euros
+
+    def __gt__(self, other):
+        if self.__euros == other.__euros:
+            return self.__cents > other.__cents
+        return self.__euros > other.__euros
+    
+    def __add__(self, other):
+        total = Money(0, 0)
+        total.__euros = self.__euros+other.__euros
+        total.__cents = self.__cents+other.__cents
+
+        if total.__cents>=100:
+            total.__euros+=1
+            total.__cents-=100
+
+        return total
+    
+    def __sub__(self, other):
+        total = Money(0, 0)
+        total.__euros = self.__euros-other.__euros
+        total.__cents = self.__cents-other.__cents 
+        if total.__euros<0 or total.__euros==0 and total.__cents<0:
+            raise ValueError(f"a negative result is not allowed")
+        if total.__cents<0:
+            total.__euros-=1
+            total.__cents+=100
+
+        return total       
+
 
--- vscode/mooc-programming-26/part10-08_simple_date/src/simple_date.py
+++ vscode/mooc-programming-26/part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,50 @@
 # WRITE YOUR SOLUTION HERE:
+class SimpleDate:
+    def __init__(self, day: int, month: int, year: int):
+        self.day = day
+        self.month = month
+        self.year = year
 
+    def __str__(self):
+        return f"{self.day}.{self.month}.{self.year}"
+
+    def __ne__(self, value):
+        return self.day != value.day or self.month != value.month or self.year != value.year
+
+    def __eq__(self, value):
+        return self.day == value.day and self.month == value.month and self.year == value.year
+
+    def __lt__(self, other):
+        date1 = self.year + self.month*0.1 + self.day*0.01
+        date2 = other.year + other.month*0.1 + other.day*0.01
+        return date1 < date2
+
+    def __gt__(self, other):
+        date1 = self.year + self.month*0.1 + self.day*0.01
+        date2 = other.year + other.month*0.1 + other.day*0.01
+        return date1 > date2
+    
+    def __add__(self, day):
+        years=day//360+self.year
+        day%=360
+        months=day//30+self.month
+        day%=30
+        days=day+self.day
+        if days>30:
+            days-=30
+            months+=1
+        if months>12:
+            months-=12
+            years+=1
+        
+        return SimpleDate(days, months, years)
+    
+    def __sub__(self, other):
+        years=self.year-other.year
+        months=self.month-other.month
+        days=self.day-other.day
+
+        total_dif=(years*360)+(months*30)+(days)
+
+        return abs(total_dif)
+    
