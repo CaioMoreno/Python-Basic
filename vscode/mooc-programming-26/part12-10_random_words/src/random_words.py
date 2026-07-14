93a23ed6380914703275b20eabdc8c7d79705509
Caio <caiomoreno.caio@gmail.com>
2026-07-13 23:11:26 -0300

    New activities

--- vscode/mooc-programming-26/part12-10_random_words/src/random_words.py
+++ vscode/mooc-programming-26/part12-10_random_words/src/random_words.py
@@ -1,3 +1,6 @@
 # Write your solution here:
+from random import choices 
 def word_generator(characters: str, lenght: int, amount: int):
-    
+    return ("".join(choices(characters, k=lenght)) for i in range(amount))
+
+ 
